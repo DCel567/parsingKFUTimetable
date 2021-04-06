5b1ad77c207cb108099a0c1646382623ff8cd0a4
George <dcel567@gmail.com>
2021-04-06 14:46:48 +0300

    added .idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import selenium.webdriver
 import selenium.webdriver.common.keys as keys
-import bs4
+from bs4 import BeautifulSoup
 import time
 
 
@@ -12,4 +12,21 @@ if __name__ == "__main__":
 	input_field.send_keys(keys.Keys.ENTER)
 
 	#  got into timetable
+	time.sleep(2)
+
+	HTMLPage = driver.page_source
+	#  print(HTMLPage)
+
+	soup = BeautifulSoup(HTMLPage, 'html5lib')
+	table = soup.findChildren('table')
+	myTable = table[0]
+
+	rows = myTable.findChildren(['th', 'tr'])
+	for row in rows:
+		cells = row.findChildren('td')
+		for cell in cells:
+			value = cell.text
+			print(value)
+			print()
+
 
